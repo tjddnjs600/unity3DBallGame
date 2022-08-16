54f8af79ee92d43a7465e0a9d2dcff3993c11822
tjddnjs600 <tjddnjs600@naver.com>
2022-08-16 20:42:25 +0900

    key이벤트

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -9,20 +9,20 @@ public class NewBehaviourScript : MonoBehaviour
     //오브젝트 생성시 최초 실행
     void Awake()
     {
-        Debug.Log("데이터 준비 됨");
+        //Debug.Log("데이터 준비 됨");
     }
 
     //활성화 오브젝트가 활성화 되었을때(게임을 킬때마다 실행 )
     void OnEnable()
     {
-        Debug.Log("로그인 함");
+        //Debug.Log("로그인 함");
     }
 
     // Start is called before the first frame update
     //게임 시작시 업데이트 함수 실행전 최초 1번 실행
     void Start()
     {
-        Debug.Log("게임 시작준비, 예)사냥 장비 챙김");
+        //Debug.Log("게임 시작준비, 예)사냥 장비 챙김");
     }
     
     //2.물리영역
@@ -32,26 +32,51 @@ public class NewBehaviourScript : MonoBehaviour
     void FixedUpdate()
     {
         //물리연산과 관련된 로직들이 보통 들어감
-        Debug.Log("플레이어 이동");
+        //Debug.Log("플레이어 이동");
     }
 
     //매 프레임마다 호출 물리효과가 적용되지 않는 오브젝트 움직임, 단순한 타이머 동작시 실행
     void Update()
     {
-        Debug.Log("몬스터 사냥");
+        //Debug.Log("몬스터 사냥");
+        
+        //Input  게임내 입력을 관리하는 클래스
+        if (Input.anyKeyDown) //입력이 되었을때
+        {
+            Debug.Log("key 눌림");
+        }
+
+        //입력중일때
+        //if (Input.anyKey) Debug.Log("key 누르는중");
+
+        
+        //변수로는 KeyCode <- 클래스가 들어감 예) KeyCode.Return <- 엔터 입력
+        //키 눌렸을때
+        //Input.GetKeyDown();
+        
+        //키 누르고 있을때
+        //Input.GetKey();
+        
+        //키 눌렀다 땠을 때
+        //Input.GetKeyUp();
+
+        //if (Input.GetKeyDown(KeyCode.A)) Debug.Log("엔터키 입력");
+        
+        //마우스 클릭 이벤트
+        //if(Input.GetMouseButtonDown(0)) Debug.Log("죄클릭 입력");
     }
 
     //모든 Update 함수가 호출된 후, 마지막으로 호출 주로 오브젝트를 따라가게 설정한 카메라는 LateUpdate 를 사용
     //카메라가 따라가는 오브젝트가 Update함수 안에서 움직일 경우가 있기 때문
     void LateUpdate()
     {
-        Debug.Log("경험치 획득, 카메라 이동");
+        //Debug.Log("경험치 획득, 카메라 이동");
     }
 
     //비활성화(게임을 끌때)
     void OnDisable()
     {
-        Debug.Log("로그아웃 함");
+        //Debug.Log("로그아웃 함");
     } 
 
 
@@ -60,6 +85,6 @@ public class NewBehaviourScript : MonoBehaviour
     //게임 오브젝트가 삭제될 때 
     void OnDestroy()
     {
-        Debug.Log("캐릭터 삭제");
+        //Debug.Log("캐릭터 삭제");
     }
 }
